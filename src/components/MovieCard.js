37238009db470518a16dce0fc2c3a04ebcde4e3c
Mohamed Tanash <tanashkoo@gmail.com>
2022-10-16 12:41:38 +0200

    add framer motion package add new animations

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,25 @@
+import { motion } from "framer-motion";
 import moment from "moment/moment";
 import { Link } from "react-router-dom";
 import truncateString from "../helpers/truncateString";
 import styles from "./MovieCard.module.css";
-import Rate from "./Rate";
+import Vote from "./Vote";
 
 const MovieCard = ({ movie }) => {
   const { id, title, poster_path, vote_average, release_date } = movie;
   return (
-    <div key={id} className={styles.movieCard}>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      variants={{
+        open: { opacity: 1, x: 0 },
+        closed: { opacity: 0, x: "-100%" },
+      }}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.95 }}
+      key={id}
+      className={styles.movieCard}
+    >
       <Link to={`/movies/${id}`}>
         <div className={styles.imageContainer}>
           <img
@@ -15,7 +27,7 @@ const MovieCard = ({ movie }) => {
             src={`https://image.tmdb.org/t/p/w200/${poster_path}`}
             alt={title}
           />
-          <Rate voteAverage={vote_average} className={styles.vote} />
+          <Vote voteAverage={vote_average} className={styles.vote} />
           <p className={styles.movieDate}>
             {moment(release_date).format("YYYY")}
           </p>
@@ -23,7 +35,7 @@ const MovieCard = ({ movie }) => {
       </Link>
 
       <h3 className={styles.movieTitle}>{truncateString(title, 30)}</h3>
-    </div>
+    </motion.div>
   );
 };
 
