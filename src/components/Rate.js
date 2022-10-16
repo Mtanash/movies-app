530dafb3dfb4b9345d8dffb0df08ccb403627986
Mohamed Tanash <tanashkoo@gmail.com>
2022-10-16 12:39:19 +0200

    change rate style

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -1,12 +1,18 @@
 import { AiFillStar } from "react-icons/ai";
+import kFormatter from "../helpers/Kformatter";
 import roundToOneDecimal from "../helpers/roundToOneDecimal";
 import styles from "./Rate.module.css";
 
-const Rate = ({ voteAverage, className }) => {
+const Rate = ({ voteAverage, className, full = false, voteCount }) => {
   return (
     <div className={className}>
       <AiFillStar className={styles.rateIcon} />
       {roundToOneDecimal(voteAverage)}
+      {full && (
+        <p className={styles.rateFraction}>
+          /10 {voteCount && `- ${kFormatter(voteCount)}`}
+        </p>
+      )}
     </div>
   );
 };
